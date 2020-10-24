0bcf07c27aa5bca023e6836f7244caa0e0a538b1
gmeghajain <gmeghajain@gmail.com>
2020-10-24 19:17:27 +0530

    First commit

---
+++ Person.java
@@ -0,0 +1,39 @@
+package com.example.AddressBookProb
+
+        
+public class Person {
+    String firstName;
+    String lastName;
+    long phone;
+    String city;
+    String state;
+    int zip;
+
+    public Person(String firstName, String lastName, long phone, String city,
+                  String state, int zip) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+    public class Person {
+        String firstName;
+        String lastName;
+        long phone;
+        String city;
+        String state;
+        int zip;
+
+        public Person(String firstName, String lastName, long phone, String city,
+                      String state, int zip) {
+            super();
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phone = phone;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+        }
